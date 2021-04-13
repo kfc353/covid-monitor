2a7c64c650403b4c72b6223a3a8ca8cf35cb532d
John <j_paraga@live.concordia.ca>
2021-04-13 16:05:06 -0400

    basic styling and barebones page

---
+++ scripts/mainCRUDPage.js
@@ -0,0 +1,16 @@
+// displays the appropriate information based on the query
+function createEntry(){
+    console.log("HELLO");
+}
+
+// deletes selected entries
+function deleteEntry(){
+
+}
+
+
+// 
+function updateEntry(){
+
+}
+
