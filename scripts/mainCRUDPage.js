0ecedcb78101f5178b236622fb184178d406d33e
John <j_paraga@live.concordia.ca>
2021-04-13 18:14:10 -0400

    added boilerplate for all requirements

--- scripts/mainCRUDPage.js
+++ scripts/mainCRUDPage.js
@@ -1,16 +1,71 @@
 // displays the appropriate information based on the query
-function createEntry(){
+function createEntry() {
     console.log("HELLO");
 }
 
 // deletes selected entries
-function deleteEntry(){
+function deleteEntry() {
 
 }
 
 
 // 
-function updateEntry(){
+function updateEntry() {
 
 }
 
+// displays the selected option's management prompts
+function displayManagementPage() {
+    var selectedOption = document.getElementById("managementSelect").value;
+    switch (selectedOption) {
+        case "person":
+            console.log("person");
+            break;
+        case "pubHealthWorker":
+            console.log("pubHealthWorker");
+            break;
+        case "facility":
+            console.log("facility");
+            break;
+        case "region":
+            console.log("region");
+            break;
+        case "groupZone":
+            console.log("groupZone");
+            break;
+        case "pubHealthRecommend":
+            console.log("pubHealthRecommend");
+            break;
+        case "address":
+            console.log("address");
+            break;
+    }
+}
+
+// displays the results of the selected query
+function displayDatabaseSearch() {
+    var selectedOption = document.getElementById("dbSearches").value;
+    switch (selectedOption) {
+        case "facilities":
+            console.log("facilities");
+            break;
+        case "regions":
+            console.log("regions")
+            break;
+        case "peopleAtAddress":
+            console.log("peopleAtAddress")
+            break;
+        case "workersAtFacility":
+            console.log("workersAtFacility")
+            break;
+        case "positiveWorkers":
+            console.log("positiveWorkers")
+            break;
+        case "regionReport":
+            console.log("regionReport")
+            break;
+        case "messages":
+            console.log("messages")
+            break;
+    }
+}
