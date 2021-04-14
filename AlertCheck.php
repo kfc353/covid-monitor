14cc8f5e28b936d30d5029d01d203d8648daf01d
AFulleringer <alexfulleringer@gmail.com>
2021-04-14 15:59:07 -0400

    Hugo/db config (#4)
    * In Progress for Alex's scripts

--- AlertCheck.php
+++ AlertCheck.php
@@ -1,5 +1,6 @@
 <?php
-// TODO: should set $host based on environment
+// Alexander Fulleringer 40005290
+// Prints out complete Alert History.
 // had a 127.0.0.1:3307 -> mysqlUrl:3306 port forwarding
 $host = "127.0.0.1:3307";
 $database = "kfc353_4";
@@ -10,11 +11,15 @@ $password = "Al3xB3st";
 
 $mysqli = new mysqli($host, $username, $password, $database);
 echo $mysqli->host_info . "\n";
-$result = $mysqli->query("SELECT * FROM Person");
 
+$result = $mysqli->query("SELECT * FROM AlertHistory");
 for ($i = 0; $i < $result->num_rows; $i++) {
   $result->data_seek($i);
   $row = $result->fetch_assoc();
   echo "<br>";
-  echo " medicareNum = " . $row['medicareNum'] . "\n";
+  echo " ID = " . $row['id'] . "\n";
+  echo " level = " . $row['level'] . "\n";
+  echo " alertDateTime = " . $row['alertDateTime'] . "\n";
+  echo " region = " . $row['region'] . "\n";
+
 }
---
+++ AlertSetup.php
@@ -0,0 +1,35 @@
+<?php
+// Alexander Fulleringer 40005290
+//Takes all Regions and sets their alert to level 1, then read out the AlertHistory
+// had a 127.0.0.1:3307 -> mysqlUrl:3306 port forwarding
+$host = "127.0.0.1:3307";
+$database = "kfc353_4";
+$username = "kfc353_4";
+$password = "Al3xB3st";
+
+// create connection
+
+$mysqli = new mysqli($host, $username, $password, $database);
+echo $mysqli->host_info . "\n";
+
+// $regions = $mysqli->query("SELECT DISTINCT region FROM CityRegion");
+$result = $mysqli->query("SELECT DISTINCT region FROM CityRegion");
+for ($i = 0; $i < $result->num_rows; $i++) {
+  $result->data_seek($i);
+  $row = $result->fetch_assoc();
+  $region = $row['region'];
+  $mysqli->query("INSERT INTO AlertHistory (level, alertDateTime, region) VALUES (1, now(), " . $region ." );");
+
+}
+
+
+$result = $mysqli->query("SELECT * FROM AlertHistory");
+for ($i = 0; $i < $result->num_rows; $i++) {
+  $result->data_seek($i);
+  $row = $result->fetch_assoc();
+  echo "<br>";
+  echo " level = " . $row['level'] . "\n";
+  echo " alertDateTime = " . $row['alertDateTime'] . "\n";
+  echo " region = " . $row['region'] . "\n";
+
+}
