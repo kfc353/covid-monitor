d4c83d936d8f4a26039b8eb69a58ab9f3ccc5d1c
John Paragas <63882260+johndwight@users.noreply.github.com>
2021-04-14 15:06:12 -0400

    basic functionalities for UI (#3)
    * add stuff in gitignore
    
    * db connect
    
    * basic styling and barebones page
    
    * added boilerplate for all requirements
    
    * placeholders with dummy functions
    
    Co-authored-by: Hugo Zheng <zhenghugo515@gmail.com>

---
+++ testing.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html>
+<?php
+// TODO: login page
+// TODO: fill-up form page
+// TODO: search page for required queries
+// TODO: interface for required queries
+// TODO: current public health information and recommendations page
+// TODO:
+// TODO:
+// TODO:
+// TODO:
+// TODO:
+
+
+
+?>
+</html>
