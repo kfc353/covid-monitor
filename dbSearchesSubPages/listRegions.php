5a16655fe26fdfe08d3c6ca150734bd86cf0ed64
johndwight <j_paraga@live.concordia.ca>
2021-04-18 12:29:53 -0400

    all dbsearches working except region report + positive workers

--- dbSearchesSubPages/listRegions.php
+++ dbSearchesSubPages/listRegions.php
@@ -1,5 +1,73 @@
 <?php
+// had a 127.0.0.1:3307 -> mysqlUrl:3306 port forwarding
+$host = "127.0.0.1:3307";
+$database = "kfc353_4";
+$username = "kfc353_4";
+$password = "Al3xB3st";
 
-echo "<div id='listRegions' style='display: none'>";
+// create connection
+$mysqli = new mysqli($host, $username, $password, $database);
+// Lists all the regions (Number 13 from project guideline)
+function listOfAllRegions($mysqli)
+{
 
-echo "</div>";
+    $regions = $mysqli->query("SELECT DISTINCT region FROM CityRegion");
+
+    // looping regions
+    if ($regions->num_rows > 0) {
+        for ($i = 0; $i < $regions->num_rows; $i++) {
+
+            $regions->data_seek($i);
+            $row = $regions->fetch_assoc();
+            $x = $i + 1;
+
+            $cities = $mysqli->query("SELECT city FROM CityRegion WHERE region ='" . $row['region'] . "'");
+           
+            
+            // prints out the region 
+            echo nl2br("<h3>Region $x " . $row['region'] . "</h3>"); // writing into the webpage 
+
+            //looping cities 
+            if ($cities->num_rows > 0) {
+
+                for ($k = 0; $k < $cities->num_rows; $k++) {
+                    $cities->data_seek($k);
+                    $row2 = $cities->fetch_assoc();
+                    $y = $k + 1;
+
+                    $postalCodes = $mysqli->query("SELECT postalCode FROM PostalCodeCity WHERE city ='" . $row2['city'] . "'");
+                    // prints out the city in every region
+                    echo "<table><caption>City $y:" . $row2['city'] . "</caption>";
+                    // looping postalCodes
+                    if ($postalCodes->num_rows > 0) {
+                        echo "<tr><th>Postal Code</th></tr>";
+                        for ($p = 0; $p < $postalCodes->num_rows; $p++) {
+                            $postalCodes->data_seek($p);
+                            $row3 = $postalCodes->fetch_assoc();
+                            $z = $p + 1;
+                            echo "<tr><td>" . $row3['postalCode'] . "</td></tr>";
+                        }
+                    } else {
+
+                        // no postal codes are found in a specific city 
+                        echo nl2br("0 Postal Codes found \n ");
+                    }
+                    echo "</table>";
+                }
+            } else {
+
+                // no cities found in this specific region 
+                echo nl2br("0 Cities found \n ");
+            }
+            echo "</table>";
+        }
+    } else {
+
+        // no regions found 
+        echo ("0 regions found ");
+    }
+}
+?>
+<div id='listRegions' style='display: none'>
+    <?php listOfAllRegions($mysqli) ?>
+</div>
--- dbSearchesSubPages/listWorkersAtFacility.php
+++ dbSearchesSubPages/listWorkersAtFacility.php
@@ -1,5 +1,76 @@
 <?php
+// had a 127.0.0.1:3307 -> mysqlUrl:3306 port forwarding
+$host = "127.0.0.1:3307";
+$database = "kfc353_4";
+$username = "kfc353_4";
+$password = "Al3xB3st";
 
-echo "<div id='listWorkersAtFacility' style='display: none'>";
+// create connection
+$mysqli = new mysqli($host, $username, $password, $database);
+// Lists all health workers in a specific facility (Number 15 from project guideline)
+function listOfHealthWorkersInSpecificFacility($mysqli, $specificFacility){
 
-echo "</div>";
+    $HealthWorkersInSpecificFacility = $mysqli->query("SELECT * FROM Person WHERE medicareNum
+    IN(SELECT DISTINCT healthWorkerMedicareNum FROM Service 
+    WHERE healthFacilityName IN(SELECT name from HealthFacility WHERE name='" . $specificFacility . "'))");
+    echo "<h3>Workers at $specificFacility</h3>";
+    // Looping through all the health workers 
+    if($HealthWorkersInSpecificFacility->num_rows > 0){
+    
+      for ($i = 0; $i < $HealthWorkersInSpecificFacility->num_rows; $i++) {
+        $HealthWorkersInSpecificFacility->data_seek($i);
+        $row = $HealthWorkersInSpecificFacility->fetch_assoc();
+        $x = $i + 1;
+  
+        // prints out the info of the health worker 
+        echo nl2br("Medicare Num $x= " . $row['medicareNum'] . "\t" . " First Name = " . $row['firstName'] . "\t" . " Last Name = " . $row['lastName'] . "\t" . " Date Of Birth = " . $row['dateOfBirth'] . 
+        "\t" . " Phone Number = " . $row['phoneNum'] . "\t" . " Address = " . $row['address']  . "\t" . " Province = " . $row['province'].  "\t" . " Citizenship = " . $row['citizenship']
+        . "\t" . " Email = " . $row['email']); // writing into the webpage 
+   
+  
+        $motherName = $mysqli->query("SELECT firstName , lastName FROM Person WHERE medicareNum = '" . $row['motherMedicareNum'] . "'");
+        $FatherName = $mysqli->query("SELECT firstName , lastName FROM Person WHERE medicareNum = '" . $row['fatherMedicareNum'] . "'");
+  
+        $motherName->data_seek($i);
+        $FatherName->data_seek($i);
+        $row2 = $motherName->fetch_assoc();
+        $row3 = $FatherName->fetch_assoc();
+  
+        // if both mother and father are available 
+        if(isset($row2) && isset($row3)){
+  
+          echo nl2br("\nMother's Name = " . $row2['firstName'] . " " . $row2['lastName'] . "\nFather's Name = " . $row3['firstName'] . " " . $row3['lastName'] . "\n\n");
+          
+          // if mother is available but father isnt 
+        }elseif(isset($row2) && !isset($row3)){
+  
+          echo nl2br("\nMother's Name = " . $row2['firstName'] . " " . $row2['lastName'] . "\nFather's Name = N/A\n\n");
+          
+          // if father is available but father isnt 
+        }elseif(!isset($row2) && isset($row3)){
+  
+          echo nl2br("\nMother's Name = N/A " . "\nFather's Name = " . $row3['firstName'] . " " . $row3['lastName'] . "\n\n");
+         
+          // if neither mother nor father are available 
+        }else{
+  
+          echo nl2br("\nMother's Name = N/A " . "\nFather's Name = N/A\n\n");
+  
+        }
+       
+      }
+  
+    }else{
+    
+      echo("0 health workers ");
+  
+    }
+  }?>
+<div id='listWorkersAtFacility' style='display: none'>
+
+<?php 
+$specificFacility = "Viau Public Health Center";
+listOfHealthWorkersInSpecificFacility($mysqli, $specificFacility);
+
+?>
+</div>
