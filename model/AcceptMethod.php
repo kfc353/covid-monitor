1f6ec0ad5d9211551369781edc17cfa3a333ee9b
Hugo Zheng <zhenghugo515@gmail.com>
2021-04-18 00:56:35 -0400

    Fix enum load

--- model/AcceptMethod.php
+++ model/AcceptMethod.php
@@ -1,8 +1,11 @@
 <?php
 
+require_once "vendor/autoload.php";
+
 use MyCLabs\Enum\Enum;
 
-final class AcceptMethod extends Enum{
+final class AcceptMethod extends Enum
+{
     private const appointment = "appointment only";
     private const walkin = "walk-in";
     private const both = "both";
