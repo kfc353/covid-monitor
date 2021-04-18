32639913b412fe0f6540cdab7a320fd143580b4d
zhengHugo <37107000+zhengHugo@users.noreply.github.com>
2021-04-17 23:00:04 -0400

    Add vendor library folder (#20)

---
+++ vendor/composer/installed.php
@@ -0,0 +1,33 @@
+<?php return array (
+  'root' => 
+  array (
+    'pretty_version' => 'dev-main',
+    'version' => 'dev-main',
+    'aliases' => 
+    array (
+    ),
+    'reference' => '3914fbc0ad629d9c28b72d54b512b06009603f53',
+    'name' => 'hugo/covid-monitor',
+  ),
+  'versions' => 
+  array (
+    'hugo/covid-monitor' => 
+    array (
+      'pretty_version' => 'dev-main',
+      'version' => 'dev-main',
+      'aliases' => 
+      array (
+      ),
+      'reference' => '3914fbc0ad629d9c28b72d54b512b06009603f53',
+    ),
+    'myclabs/php-enum' => 
+    array (
+      'pretty_version' => '1.8.0',
+      'version' => '1.8.0.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => '46cf3d8498b095bd33727b13fd5707263af99421',
+    ),
+  ),
+);
