fc69f920c15e3eabbe2de6647978478ee8161a7f
johndwight <j_paraga@live.concordia.ca>
2021-04-21 16:36:12 -0400

    fixed alertcheck.php

--- alertsAndMessagesSubPages/AlertCheck.php
+++ alertsAndMessagesSubPages/AlertCheck.php
@@ -1,16 +1,10 @@
 <div id="createTablesDiv" style="display: none;">
 <?php
-// Alexander Fulleringer 40005290
-// Prints out complete Alert History.
-// had a 127.0.0.1:3307 -> mysqlUrl:3306 port forwarding
-$host = "127.0.0.1:3307";
-$database = "kfc353_4";
-$username = "kfc353_4";
-$password = "Al3xB3st";
+
+$mysqli = MysqlConnection::getInstance()->getMysqli();
 
 // create connection
 
-$mysqli = new mysqli($host, $username, $password, $database);
 
 $result = $mysqli->query("SELECT * FROM AlertHistory");
 for ($i = 0; $i < $result->num_rows; $i++) {
